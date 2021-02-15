117102777831bb176a6ebbd02060bb2b59b693ed
Eric Johnston <ej@machinelevel.com>
2021-02-15 15:51:43 -0500

    [magtag] first magtag epd success

--- pinup-weather.py
+++ pinup-weather.py
@@ -15,102 +15,311 @@ License:
 
 my notes:
 # deep sleep: https://learn.adafruit.com/deep-sleep-with-circuitpython
-  """
- 
-import time
-import busio
+# best magtag wifi sample: https://learn.adafruit.com/magtag-progress-displays?view=all
+
+"""
+
+is_magtag = True
+use_magtag_lib = False
+
+def check_display(msg, do_loop=4):
+    while do_loop > 0:
+        print('Display:', msg, do_loop)
+        print(type(board.DISPLAY))
+        print(board.DISPLAY)
+        time.sleep(board.DISPLAY.time_to_refresh)
+        while board.DISPLAY.busy:
+            time.sleep(1)
+        time.sleep(board.DISPLAY.time_to_refresh)
+        board.DISPLAY.refresh()
+        do_loop -= 1
+    print('...ok!')
+
+"""
+# This sample causes a refresh! May need to reset board first
 import board
+import time
 import displayio
-import adafruit_il0373
-import gc
-import neopixel
+#displayio.release_displays()
+
+display = board.DISPLAY
+print(display)
+time.sleep(display.time_to_refresh)
+time.sleep(display.time_to_refresh)
+display.refresh()
+print('ok!')
+"""
+
+import time
 import adafruit_imageload
 
-import adafruit_requests as requests
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi
-from digitalio import DigitalInOut
+if use_magtag_lib:
+    from adafruit_magtag.magtag import MagTag
+else:
+    import busio
+    import board
+    check_display(39)
+    import analogio
+    import gc
+    import neopixel
+    if not is_magtag:
+        import adafruit_il0373
+        import adafruit_requests as requests
+        import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+        from adafruit_esp32spi import adafruit_esp32spi
+        # This pinout works on a Feather M4 and may need to be altered for other boards.
+        spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    from digitalio import DigitalInOut
+    pixels = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2, auto_write=True)
+    check_display(51)
+
 try:
     from secrets import secrets
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-displayio.release_displays()
-# This pinout works on a Feather M4 and may need to be altered for other boards.
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+import displayio
+#displayio.release_displays()
+
 zipcode = '02210' # This is the zip code it will report weather for
-show_current_temp = None
-show_feels_like_temp = None
 
 def main():
-    ink = Ink()
-    ink.draw_all()
+    if use_magtag_lib:
+        magtag = MagTag()
+    else:
+        magtag = None
+
+    ink = Ink(magtag)
+#    ink.draw_all(None)
     time.sleep(1)
-    weather = NetWeather()
+#    weather = NetWeather()
+    weather = None
     time.sleep(1)
     while 1:
-        weather.fetch_weather()
-        ink.draw_all()
-        time.sleep(30 * 60)
+#        weather.fetch_weather()
+        pixels[0] = (0,0,0)
+        ink.draw_all(weather)
+        time.sleep(15 * 60)
 
 
 class Ink:
-    def __init__(self):
+    def __init__(self, magtag):
         print('initializing ink...')
-        epd_cs = board.D9
-        epd_dc = board.D10
- 
-        display_bus = displayio.FourWire(
-            spi, command=epd_dc, chip_select=epd_cs, baudrate=1000000
-        )
-        time.sleep(1)
-         
-        self.display = adafruit_il0373.IL0373(
-            display_bus,
-            width=296,
-            height=128,
-            rotation=270,
-            black_bits_inverted=False,
-            color_bits_inverted=False,
-            grayscale=True,
-            refresh_time=1,
-        )
+        self.magtag = magtag
+        if self.magtag:
+            print('>>>>>>>>>>>>>')
+            print(type(magtag.graphics))
+            print(type(magtag.graphics.display))
+            pass
+        elif is_magtag:
+            epd_cs = board.EPD_CS
+            epd_dc = board.EPD_DC
+        else:
+            epd_cs = board.D9
+            epd_dc = board.D10
+#        self.batt_pin = analogio.AnalogIn(board.A5)
+        
+        if is_magtag:
+            self.display = board.DISPLAY
+            time.sleep(self.display.time_to_refresh)
+            time.sleep(self.display.time_to_refresh)
+            self.display.refresh()
+            time.sleep(self.display.time_to_refresh)
+            time.sleep(self.display.time_to_refresh)
+            self.display.refresh()
+            time.sleep(self.display.time_to_refresh)
+            time.sleep(self.display.time_to_refresh)
+            print('at 110')
+        else:
+            display_bus = displayio.FourWire(
+                spi, command=epd_dc, chip_select=epd_cs, baudrate=1000000
+            )
+            time.sleep(1)
+            self.display = adafruit_il0373.IL0373(
+                display_bus,
+                width=296,
+                height=128,
+                rotation=270,
+                black_bits_inverted=False,
+                color_bits_inverted=False,
+                grayscale=True,
+                refresh_time=1,
+            )
 
 
-        f = open("/images/pin2c.bmp", "rb")
-        pic = displayio.OnDiskBitmap(f)
-        self.pinback1 = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
-        self.numbers22 = numbers_from_bmp('/images/numbers22x165.bmp', 8, 15)
-        self.numbers17 = numbers_from_bmp('/images/numbers17x127.bmp', 6, 12)
-        self.numbers11 = numbers_from_bmp('/images/numbers11x82.bmp', 2, 8)
+        f = open("/images/pin2d.bmp", "rb")
+        self.pinback1 = displayio.OnDiskBitmap(f)
+        self.numbers22 = self.numbers_from_bmp('/images/numbers22x165.bmp', '-0123456789', [0,8,25,40,56,71,87,103,118,134,149,165])
+        self.numbers17 = self.numbers_from_bmp('/images/numbers17x127.bmp', '-0123456789', [0,6,19,31,43,54,67, 79, 91,103,114,127])
+        self.numbers11 = self.numbers_from_bmp('/images/numbers11x82.bmp',  '-0123456789', [0,3,12,19,27,34,43, 50, 58, 66, 73, 81])
+        self.weekdays11 = self.numbers_from_bmp('/images/weekdays11x174.bmp', '0123456', [0,31,55,85,112,131,155,174])
+        self.months11 = self.numbers_from_bmp('/images/months11x294.bmp', '0123456789abc', [0,0,22,44,73,98,126,149,170,197,220,244,272,294])
+        self.batt20 = self.numbers_from_bmp('/images/batt20x118.bmp', '012c', [0,28,58,87,118])
+        self.palette1 = displayio.Palette(4)
+        self.palette1[0] = 0x000000
+        self.palette1[1] = 0x606060
+        self.palette1[2] = 0x909090
+        self.palette1[3] = 0xffffff
+        self.palette1.make_transparent(3)
+        print('initialized ink ok.')
 
 #        t8 = tile_from_bmp('/images/icon_suncloud64_8.bmp')
 
-    def draw_all(self):
-        g = displayio.Group()
-        g.append(self.pinback1)
+    def draw_all(self, weather):
+        print('drawing...')
+        g = displayio.Group(max_size=30)
+        g.append(displayio.TileGrid(self.pinback1, pixel_shader=displayio.ColorConverter()))
 
-        self.draw_number(show_current_temp, 50, 50, self.numbers22, g)
+#        show_current_temp = -8
+#        show_feels_like_temp = -12
+        if weather is not None:
+            self.draw_number(weather.show_current_temp, 140, 80, self.numbers22, g)
+            self.draw_number(weather.show_feels_like_temp, 116, 90, self.numbers17, g)
+            x = 50
+            y = 0
+            y = self.draw_number(weather.updated_hour, x, y, self.numbers11, g)
+            y = self.draw_number(weather.updated_minute, x, y + 5, self.numbers11, g)
+            y = self.draw_index([weather.updated_weekday], x, y + 5, self.weekdays11, g)
+            y = self.draw_index([weather.updated_month], x, y + 5, self.months11, g)
+            y = self.draw_number(weather.updated_monthday, x, y + 5, self.numbers11, g)
+
+        self.draw_battery_level(g)
 
         self.display.show(g)
         self.display.refresh()
+        print('  ...draw done.')
         g = None
         gc.collect()
 
-    def draw_number(self, val, x, y, font, group):
-#        if val is not None:
-        group.append(font[2])
+    def draw_battery_level(self, g):
+        return
+        battery_voltage = (self.batt_pin.value * 3.3) / 65536 * 2
+        print("VBat voltage: {:.2f}".format(battery_voltage))
+        x = 0
+        y = 0
+        y = self.draw_index([2], x, y, self.batt20, g)
+        y = self.draw_number(int(battery_voltage * 100), x, y + 5, self.numbers11, g)
 
 
+    def draw_number(self, val, x, y, font, group):        
+        if val is not None:
+            for ch in str(val):
+                bm = font[ch]
+                tile = displayio.TileGrid(bm, pixel_shader=self.palette1, x=x, y=y)
+                group.append(tile)
+                y += bm.height
+        return y
 
+    def draw_index(self, val, x, y, font, group):        
+        if val is not None:
+            for i in val:
+                bm = font[i]
+                tile = displayio.TileGrid(bm, pixel_shader=self.palette1, x=x, y=y)
+                group.append(tile)
+                y += bm.height
+        return y
+
+    def tile_from_bmp(self, file_name):
+        dbm, dpal = adafruit_imageload.load(file_name,
+                                            bitmap=displayio.Bitmap,
+                                            palette=displayio.Palette)
+        bm = displayio.Bitmap(dbm.width, dbm.height, 4)
+        for y in range(dbm.height):
+            for x in range(dbm.width):
+                bm[x, y] = self.gray_256_to_4(dpal[dbm[x, y]] & 0x00ff)
+        dbm = None
+        dpal = None
+        gc.collect()
+        return bm
+
+    def gray_256_to_4(self, p):
+        if p < 0x60:
+            return 0
+        elif p < 0x90:
+            return 1
+        elif p < 0xff:
+            return 2
+        return 3
+
+    def numbers_from_bmp(self, file_name, symbols, markers):
+        dbm, dpal = adafruit_imageload.load(file_name,
+                                            bitmap=displayio.Bitmap,
+                                            palette=displayio.Palette)
+        numbers = {}
+        # a = 0
+        # b = markers[0]
+        # bm = displayio.Bitmap(dbm.width, b - a, 4)
+        # for y in range(bm.height):
+        #     for x in range(bm.width):
+        #         bm[x, y] = self.gray_256_to_4(dpal[dbm[x, y]] & 0x00ff)
+        # numbers['-'] = numbers[-1] = bm
+        for index,symbol in enumerate(symbols):
+            a = markers[index]
+            b = markers[index + 1]
+            bm = displayio.Bitmap(dbm.width, b - a, 4)
+            for y in range(bm.height):
+                for x in range(bm.width):
+                    bm[x, y] = self.gray_256_to_4(dpal[dbm[x, y + a]] & 0x00ff)
+            numbers[symbol] = numbers[index] = bm
+
+        dbm = None
+        dpal = None
+        gc.collect()
+        return numbers
+
+# Magtag pins:
+# ['__class__', 'A1', 'A3', 'ACCELEROMETER_INTERRUPT', 'AD1', 'BATTERY', 
+# 'BUTTON_A', 'BUTTON_B', 'BUTTON_C', 'BUTTON_D', 'CS', 'D1', 'D10', 'D11', 
+# 'D12', 'D13', 'D14', 'D15', 'DISPLAY', 'EPD_BUSY', 'EPD_CS', 'EPD_DC', 
+# 'EPD_RESET', 'I2C', 'LIGHT', 'MISO', 'MOSI', 'NEOPIXEL', 'NEOPIXEL_POWER', 
+# 'SCK', 'SCL', 'SDA', 'SPEAKER', 'SPEAKER_ENABLE', 'SPI', 'VOLTAGE_MONITOR']
+"""
+board.A1 board.AD1
+board.A3 board.LIGHT
+board.ACCELEROMETER_INTERRUPT
+board.BATTERY board.VOLTAGE_MONITOR
+board.BUTTON_A board.D15
+board.BUTTON_B board.D14
+board.BUTTON_C board.D12
+board.BUTTON_D board.D11
+board.CS board.EPD_CS
+board.D1 board.NEOPIXEL
+board.D10
+board.D13
+board.EPD_BUSY
+board.EPD_DC
+board.EPD_RESET
+board.MISO
+board.MOSI
+board.NEOPIXEL_POWER
+board.SCK
+board.SCL
+board.SDA
+board.SPEAKER
+board.SPEAKER_ENABLE
+"""
 
 class NetWeather:
     def __init__(self):
         print('initializing weather...')
-        esp32_cs = DigitalInOut(board.D13)
-        esp32_ready = DigitalInOut(board.D11)
-        esp32_reset = DigitalInOut(board.D12)
+        print(dir(board))
+        self.show_current_temp = 0
+        self.show_feels_like_temp = 0
+        self.updated_hour = 0
+        self.updated_minute = 0
+        self.updated_weekday = 0
+        self.updated_month = 1
+        self.updated_monthday = 0
+        return
+        if is_magtag:
+            esp32_cs = DigitalInOut(board.ESP_CS)
+            esp32_ready = DigitalInOut(board.ESP_BUSY)
+            esp32_reset = DigitalInOut(board.ESP_RESET)
+        else:
+            esp32_cs = DigitalInOut(board.D13)
+            esp32_ready = DigitalInOut(board.D11)
+            esp32_reset = DigitalInOut(board.D12)
         self.esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
         requests.set_socket(socket, self.esp)
         if self.esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
@@ -147,34 +356,41 @@ class NetWeather:
         time_url = "http://worldtimeapi.org/api/ip"
         print('fetching weather now...')
         weather_now_url = 'http://api.openweathermap.org/data/2.5/weather?zip={},us&units=metric&appid={}'.format(zipcode, secrets['openweather_apikey'])
-        wnow = self.fetch_json(weather_now_url)
-        tzone = wnow['timezone']
-        lat = wnow['coord']['lat']
-        lon = wnow['coord']['lon']
-        timezone = wnow['timezone']
-        time_now = time.localtime(wnow['dt'] + timezone)
-        days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        print('It\'s {}:{} on {}'.format(time_now.tm_hour, time_now.tm_min, days[time_now.tm_wday]))
-        print('Weather in {}:'.format(wnow['name']))
-        print('  temp: {}'.format(round(wnow['main']['temp'])))
-        print('  feels-like: {}'.format(round(wnow['main']['feels_like'])))
-        print('    {}'.format(wnow['weather'][0]['description']))
-        print('  wind: {} mph'.format(round(0.621371 * wnow['wind']['speed'])))
+        try:
+            wnow = self.fetch_json(weather_now_url)
+            tzone = wnow['timezone']
+            lat = wnow['coord']['lat']
+            lon = wnow['coord']['lon']
+            timezone = wnow['timezone']
+            time_now = time.localtime(wnow['dt'] + timezone)
+            days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            print('It\'s {}:{} on {}'.format(time_now.tm_hour, time_now.tm_min, days[time_now.tm_wday]))
+            print('Weather in {}:'.format(wnow['name']))
+            print('  temp: {}'.format(round(wnow['main']['temp'])))
+            print('  feels-like: {}'.format(round(wnow['main']['feels_like'])))
+            print('    {}'.format(wnow['weather'][0]['description']))
+            print('  wind: {} mph'.format(round(0.621371 * wnow['wind']['speed'])))
+            self.show_current_temp = round(wnow['main']['temp'])
+            self.show_feels_like_temp = round(wnow['main']['feels_like'])
+            self.updated_hour = time_now.tm_hour
+            self.updated_minute = time_now.tm_min
+            self.updated_weekday = time_now.tm_wday
+            self.updated_month = time_now.tm_mon
+            self.updated_monthday =time_now.tm_mday
 
-        print('fetching hourly forecast...')
-        weather_one_url = 'http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}'.format(lat, lon, secrets['openweather_apikey'])
-        wone = self.fetch_json(weather_one_url)
-        hourly = wone['hourly']
-        for item in hourly:
-            hourly_time = time.localtime(item['dt'] + timezone)
-            out_str = '  {}:{} on {}:'.format(hourly_time.tm_hour, hourly_time.tm_min, days[hourly_time.tm_wday])
-            out_str += ' {}/{}'.format(item['temp'], item['feels_like'])
-            out_str += ' {}'.format(item['weather'][0]['description'])
-            print(out_str)
-
-        show_current_temp = round(wnow['main']['temp'])
-        show_feels_like_temp = round(wnow['main']['feels_like'])
+            print('fetching hourly forecast...')
+            weather_one_url = 'http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}'.format(lat, lon, secrets['openweather_apikey'])
+            wone = self.fetch_json(weather_one_url)
+            hourly = wone['hourly']
+            for item in hourly:
+                hourly_time = time.localtime(item['dt'] + timezone)
+                out_str = '  {}:{} on {}:'.format(hourly_time.tm_hour, hourly_time.tm_min, days[hourly_time.tm_wday])
+                out_str += ' {}/{}'.format(item['temp'], item['feels_like'])
+                out_str += ' {}'.format(item['weather'][0]['description'])
+                print(out_str)
+        except:
+            print('failed to get weather forecast')
 
         wnow = None
         wone = None
@@ -214,60 +430,5 @@ class NetWeather:
         # 13d.png     13n.png     snow
         # 50d.png     50n.png     mist
 
-def tile_from_bmp(file_name):
-    pal7 = displayio.Palette(4)
-    pal7[0] = 0x000000
-    pal7[1] = 0x606060
-    pal7[2] = 0x909090
-    pal7[3] = 0xffffff
-    pal7.make_transparent(3)
-    dbm, dpal = adafruit_imageload.load(file_name,
-                                        bitmap=displayio.Bitmap,
-                                        palette=displayio.Palette)
-    bm = displayio.Bitmap(dbm.width, dbm.height, 4)
-    for y in range(dbm.height):
-        for x in range(dbm.width):
-            bm[x, y] = gray_256_to_4(dpal[dbm[x, y]] & 0x00ff)
-    dbm = None
-    dpal = None
-    gc.collect()
-    return displayio.TileGrid(bm, pixel_shader=pal7)
-
-def gray_256_to_4(p):
-    if p < 0x60:
-        return 0
-    elif p < 0x90:
-        return 1
-    elif p < 0xff:
-        return 2
-    return 3
-
-def numbers_from_bmp(file_name, minus_width, digit_width):
-    pal7 = displayio.Palette(4)
-    pal7[0] = 0x000000
-    pal7[1] = 0x606060
-    pal7[2] = 0x909090
-    pal7[3] = 0xffffff
-    pal7.make_transparent(3)
-    dbm, dpal = adafruit_imageload.load(file_name,
-                                        bitmap=displayio.Bitmap,
-                                        palette=displayio.Palette)
-    numbers = {}
-    bm = displayio.Bitmap(dbm.width, minus_width, 4)
-    for y in range(bm.height):
-        for x in range(bm.width):
-            bm[x, y] = gray_256_to_4(dpal[dbm[x, y]] & 0x00ff)
-    numbers['-'] = numbers[-1] = displayio.TileGrid(bm, pixel_shader=pal7)
-    for digit in range(10):
-        bm = displayio.Bitmap(dbm.width, digit_width, 4)
-        for y in range(bm.height):
-            for x in range(bm.width):
-                bm[x, y] = gray_256_to_4(dpal[dbm[x, y + digit * digit_width + minus_width]] & 0x00ff)
-        numbers[str(digit)] = numbers[digit] = displayio.TileGrid(bm, pixel_shader=pal7)
-
-    dbm = None
-    dpal = None
-    gc.collect()
-    return numbers
 
 main()
